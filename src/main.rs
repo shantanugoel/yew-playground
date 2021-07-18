131b571b030a1f2a7998c595cc83f0cc29aa3640
Shantanu Goel <shantanu.goel@gmail.com>
2021-07-18 22:09:36 +0530

    Working things: timezone, table, etc

---
+++ src/main.rs
@@ -0,0 +1,121 @@
+use std::fmt;
+
+use chrono::{Duration, Local, TimeZone};
+use chrono_tz::{America::Los_Angeles, Asia::Kolkata};
+use csv::ReaderBuilder;
+use serde::Deserialize;
+use yew::{
+    prelude::*,
+    services::{console, ConsoleService},
+};
+
+enum Msg {
+    AddOne,
+}
+
+#[derive(Debug, Clone, Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+struct TzData {
+    city: String,
+    country: String,
+    timezone: String,
+}
+
+impl fmt::Display for TzData {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}, {}, {}", self.city, self.country, self.timezone)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Model {
+    // `ComponentLink` is like a reference to a component.
+    // It can be used to send messages to the component
+    link: ComponentLink<Self>,
+    value: i64,
+    tz_data: Vec<TzData>,
+}
+
+impl Component for Model {
+    type Message = ChangeData;
+    type Properties = ();
+
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let data = include_str!("cities.csv");
+        let mut rdr = ReaderBuilder::new().from_reader(data.as_bytes());
+        let mut model = Self {
+            link,
+            value: 0,
+            tz_data: Vec::new(),
+        };
+
+        for result in rdr.deserialize() {
+            let record: TzData = result.unwrap();
+            model.tz_data.push(record.clone());
+            // ConsoleService::log(record.to_string().as_ref());
+        }
+        model
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        // ConsoleService::log(msg.as_ref());
+        match msg {
+            ChangeData::Files(x) => ConsoleService::log("file"),
+            ChangeData::Value(x) => ConsoleService::log(x.as_ref()),
+            ChangeData::Select(x) => ConsoleService::log("select"),
+        }
+        true
+        // match msg {
+        //     Msg::AddOne => {
+        //         self.value += 1;
+        //         // the value has changed so we need to
+        //         // re-render for it to appear on the page
+        //         true
+        //     }
+        // }
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        // let blr_time = Local::today().with_timezone(&Kolkata).and_hms(9, 0, 0);
+        let blr_time = Kolkata.ymd(2021, 7, 18).and_hms(9, 0, 0);
+        let mtv_time = blr_time.with_timezone(&Los_Angeles);
+        let range = 0..24;
+        let cities = vec!["BLR", "MTV"];
+        html! {
+            <div>
+                // <button onclick=self.link.callback(|x| x)>{ "+1" }</button>
+                <p>{ self.value } { self.tz_data.len()}</p>
+                <p>{ " Hello"}</p>
+                <p>{"Blr: "}{blr_time} {" MTV: "}{mtv_time}</p>
+                <input type="date" onchange=self.link.callback(|x| x)/>
+                <table>
+                <thead>
+                <tr>
+                {
+                for cities.iter().map(|city| {html! {<th> {city} </th>} })
+                }
+                </tr>
+                </thead>
+                {
+                    for range.map(|x| {html! {<tr>
+                        <td> { {(blr_time + Duration::hours(x)).format("%l:%M %p %e %b %Y")} } </td>
+                        <td> { {(mtv_time + Duration::hours(x)).format("%l:%M %p %e %b %Y")} } </td>
+                        </tr>}})
+                }
+                </table>
+            </div>
+        }
+    }
+}
+
+fn main() {
+    // let rdr = ReaderBuilder::new().from_path("cities.csv");
+    yew::start_app::<Model>();
+}
